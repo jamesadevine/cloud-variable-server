ecc66f79d8578233be21868558c4159480bac473
James Devine <j.devine@lancaster.ac.uk>
2018-10-23 17:44:17 +0100

    removed outdated cloud variable stuff

--- server.js
+++ server.js
@@ -4,17 +4,15 @@ var express = require('express'),
   bodyParser = require('body-parser');
 
 var http = require('http').Server(app);
-var io = require('socket.io')(http);
-
-
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-app.use(express.static("../pxt-microbit/static"))
-
 var namespace_obj = { }
 
-
+app.route('/api/data-store/energy/').post(function(req,res){
+    console.log(req.header('school-id'),req.header('pi-id'))
+    res.json("OK!");
+});
 app.route('/api/share/').get(function(req, res){
     console.log("get SHARE")
 
@@ -26,34 +24,4 @@ app.route('/api/share/').get(function(req, res){
     res.json(namespace_obj)
 });
 
-function track_changes(namespace, appId, variable_name, value)
-{
-    // create namespace if doesn't exist
-    if (!(namespace in namespace_obj))
-        namespace_obj[namespace] = {}
-
-    // create object for appId if doesn't exist
-    if (!(appId in namespace_obj[namespace]))
-        namespace_obj[namespace][appId] = {}
-
-    namespace_obj[namespace][appId][variable_name] = {
-        "value":value,
-        "timestamp": new Date().getTime()
-    }
-}
-
-io.on('connection', function(socket){
-    socket.on('variable_changed',function(msg){
-        console.log(msg)
-        track_changes(msg.namespace, msg.appId, msg.variable_name, msg.value)
-        socket.broadcast.emit('variable_changed',msg);
-    })
-    console.log('a user connected');
-});
-
-var port = process.env.PORT || 8001
-
-console.log("socket io listening on port: ", port)
 http.listen(port)
-console.log("pxt hosted on: ", 80)
-app.listen(80);
